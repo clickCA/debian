4402451415d9260cd005e183d8b3d1fbb03c17b4
chanakorn <chanakorn.aramsak@gmail.com>
2024-05-24 03:52:18 +0700

    add server installation

---
+++ server/install-with-pihole.sh
@@ -0,0 +1,15 @@
+apt install sudo vim
+sudo apt install ufw
+sudo ufw enable
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
+ufw allow 80/tcp
+ufw allow 53/tcp
+ufw allow 53/udp
+ufw allow 67/tcp
+ufw allow 67/udp
+sudo apt install curl
+sudo apt install git
+sudo apt install python3
+curl -sSL https://install.pi-hole.net | bash
