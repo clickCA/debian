02475d76e2430f02cf53e2ebe9a6656bba3093bf
chanakorn <chanakorn.aramsak@gmail.com>
2024-05-24 03:52:11 +0700

    add touchpad

---
+++ os/touchpad.sh
@@ -0,0 +1,6 @@
+# https://unix.stackexchange.com/questions/746731/add-apt-repository-command-gives-traceback-error
+sudo apt install python3-launchpadlib
+# https://github.com/JoseExposito/touchegg
+sudo add-apt-repository ppa:touchegg/stable
+sudo apt update
+sudo apt install touchegg
