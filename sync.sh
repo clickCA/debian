e482575eea1bb1c72dc528141ee052e47b39a0d5
chanakorn <chanakorn.aramsak@gmail.com>
2024-05-28 17:54:37 +0700

    config of: Tue May 28 05:54:37 PM +07 2024

--- sync.sh
+++ sync.sh
@@ -1,10 +1,11 @@
 current_date_time=$(date)
 
-mkdir dotfiles
-mkdir dotfiles/.config
-cp ~/.zshrc dotfiles
-cp ~/Pictures/backgrounds/* .
-mkdir dotfiles/.config/kitty && cp ~/.config/kitty/kitty.conf dotfiles/.config/kitty
+# mkdir dotfiles
+# mkdir dotfiles/.config
+# cp ~/.zshrc dotfiles
+cp ~/.bashrc
+cp ~/Pictures/* /background
+# mkdir dotfiles/.config/kitty && cp ~/.config/kitty/kitty.conf dotfiles/.config/kitty
 
 git add .
 git commit -m "config of: $current_date_time"
