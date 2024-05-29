7f4e0e6885f3d096c670f1815390221e2934648d
Chanakorn Aramsak <85776877+clickCA@users.noreply.github.com>
2024-05-29 23:07:32 +0700

    Update turn-on-off-monitor.sh

--- server/turn-on-off-monitor.sh
+++ server/turn-on-off-monitor.sh
@@ -1,5 +1,15 @@
-
+#!/bin/bash
 # To turn off monitor in console, the command is the following: https://askubuntu.com/questions/62858/turn-off-monitor-using-command-line
+
+
+# Check if vbetool is installed
+if ! command -v vbetool &> /dev/null; then
+  echo "vbetool not found. Installing..."
+  sudo apt-get update  # Update package lists (optional, but recommended)
+  sudo apt-get install vbetool
+else
+  echo "vbetool already installed."
+fi
 
 sudo vbetool dpms off
 # To regain control of the console on pressing Enter key, I suggest
