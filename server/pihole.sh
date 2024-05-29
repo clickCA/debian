22afb14abda19f58a002826c429a00790b33f8c4
Chanakorn Aramsak <85776877+clickCA@users.noreply.github.com>
2024-05-29 21:25:39 +0700

    Update pihole.sh

--- server/pihole.sh
+++ server/pihole.sh
@@ -4,11 +4,11 @@ sudo ufw allow ssh
 sudo ufw allow http
 sudo ufw allow https
 sudo ufw allow 22/tcp
-ufw allow 80/tcp
-ufw allow 53/tcp
-ufw allow 53/udp
-ufw allow 67/tcp
-ufw allow 67/udp
+sudo ufw allow 80/tcp
+sudo ufw allow 53/tcp
+sudo ufw allow 53/udp
+sudo ufw allow 67/tcp
+sudo ufw allow 67/udp
 sudo apt install curl
 sudo apt install git
 sudo apt install python3
