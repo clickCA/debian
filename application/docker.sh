ef712d5810ee880bfaec2cd45fe26043379bacc4
chanakorn-aramsak <chanakorn.aramsak@gmail.com>
2024-05-31 15:00:17 +0700

    add postinstall docker

--- application/docker.sh
+++ application/docker.sh
@@ -20,3 +20,18 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 # Add Docker compose plugin
 sudo apt-get install docker-compose-plugin
 docker compose version
+
+
+#To create the docker group and add your user:
+
+#Create the docker group.
+
+sudo groupadd docker
+#Add your user to the docker group.
+sudo usermod -aG docker $USER
+#Log out and log back in so that your group membership is re-evaluated.
+
+#If you're running Linux in a virtual machine, it may be necessary to restart the virtual machine for changes to take effect.
+
+#You can also run the following command to activate the changes to groups:
+newgrp docker
