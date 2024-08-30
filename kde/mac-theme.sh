91c316db04a59523f23e4465589ec2e19e6a9e0b
chanakorn <chanakorn.aramsak@gmail.com>
2024-08-30 21:50:41 +0700

    Add latte dock to theme

--- kde/mac-theme.sh
+++ kde/mac-theme.sh
@@ -2,3 +2,7 @@ git clone https://github.com/vinceliuice/MacSonoma-kde.git
 
 cd MacSonoma-kde
 ./install.sh
+
+sudo apt install latte-dock
+sudo systemctl start latte-dock
+sudo systemctl enable latte-dock
