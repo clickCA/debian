21e8e9c01f00942e39479757e93a1f47f016a9b2
Chanakorn Aramsak <85776877+clickCA@users.noreply.github.com>
2024-12-14 11:05:45 +0700

    Create git-ask-for-username-libsecret.sh

---
+++ git-ask-for-username-libsecret.sh
@@ -0,0 +1,3 @@
+sudo apt install libsecret-1-0 libsecret-1-dev libglib2.0-dev
+sudo make --directory=/usr/share/doc/git/contrib/credential/libsecret
+git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
